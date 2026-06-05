e68ee6bddbc4907e7adfd629a96c71446c1ebf01
Samia Khan <samiakn@amazon.com>
2026-06-05 03:05:32 +0100

    pushing latest changes to s3 module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -28,7 +28,7 @@ resource "aws_kms_key" "s3_kms" {
 
 resource "aws_kms_alias" "kms_alias" {
   name          = format("alias/%s-%s-%s", var.application, var.environment, var.region)
-  target_key_id = aws_kms_key.s3-kms.key_id
+  target_key_id = aws_kms_key.s3_kms.key_id
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.s3-kms.arn
+      kms_master_key_id = aws_kms_key.s3_kms.arn
       sse_algorithm     = "aws:kms"
     }
     bucket_key_enabled       = true
@@ -44,3 +44,74 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_policy" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    id = "medireports-transition-policy"
+
+    filter {}
+
+    transition {
+      days = 90
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days = 365
+      storage_class = "DEEP_ARCHIVE"
+    }    
+
+    expiration {
+      days = 2555
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 365
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
+    status = "Enabled"
+  }
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  //deny https
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.main.arn,
+      "${aws_s3_bucket.main.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
